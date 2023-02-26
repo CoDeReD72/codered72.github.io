b9d171aceba5ef926a99237a41c91023a4229cb1
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-02-26 19:20:26 +0000

    Basic CSS Applied

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
 module.exports = function(eleventyConfig){
     
+    eleventyConfig.addPassthroughCopy("src/assets/") // Copies assets through to the site build
+    eleventyConfig.addPassthroughCopy("src/css/") // Copies CSS Over
+
+    eleventyConfig.addWatchTarget("src/css") // Eleventy watches filed in /css/ for changes 
+
+
+    eleventyConfig.addCollection('posts', function (collectionApi){
+        return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md')
+    })
+
 return{
     dir: {
         input: 'src',

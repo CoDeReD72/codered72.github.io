3fdb7c3155a4d874b5ad792c3ab90d3b7363353e
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-02-27 00:19:08 +0000

    Basic Functionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
+
 module.exports = function(eleventyConfig){
     
     eleventyConfig.addPassthroughCopy("src/assets/") // Copies assets through to the site build
     eleventyConfig.addPassthroughCopy("src/css/") // Copies CSS Over
 
     eleventyConfig.addWatchTarget("src/css") // Eleventy watches filed in /css/ for changes 
-
 
     eleventyConfig.addCollection('posts', function (collectionApi){
         return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md')

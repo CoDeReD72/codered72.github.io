c5c9a8b5ed29b54c6d71d67a5af66bb405ce05ce
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-03-02 12:17:35 +0000

    Contrast Edit and Basic Linting

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,26 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/"); // Copies assets through to the site build
+  eleventyConfig.addPassthroughCopy("src/css/"); // Copies CSS Over
 
-module.exports = function(eleventyConfig){
-    
-    eleventyConfig.addPassthroughCopy("src/assets/") // Copies assets through to the site build
-    eleventyConfig.addPassthroughCopy("src/css/") // Copies CSS Over
+  eleventyConfig.addWatchTarget("src/css"); // Eleventy watches filed in /css/ for changes
 
-    eleventyConfig.addWatchTarget("src/css") // Eleventy watches filed in /css/ for changes 
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/blog/posts/**/*.md");
+  });
 
-    eleventyConfig.addCollection('posts', function (collectionApi){
-        return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md')
-    })
+  eleventyConfig.addCollection("recentPosts", (collection) =>
+    collection.getFilteredByGlob("src/blog/posts/**/*.md").reverse().slice(0, 5)
+  );
 
-return{
+  return {
     dir: {
-        input: 'src',
-        includes: '_includes',
-        outputs: '_site',
+      input: "src",
+      includes: "_includes",
+      outputs: "_site",
     },
-    templateFormats: ['md', 'njk', 'html'],
-    markdownTemplates: 'njk',
-    htmlTemplateEngine: 'njk',
-    dataTemplateEngine: 'njk',
-}
-
-}
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplates: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};

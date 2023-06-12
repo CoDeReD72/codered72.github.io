f39cb022b9a9f6bdae29481e7a922b7850aa8b2d
CoDeReD72 <47104283+CoDeReD72@users.noreply.github.com>
2023-06-12 04:01:52 +0100

    Different approach to build

--- .eleventy.js
+++ .eleventy.js
@@ -15,42 +15,11 @@ module.exports = function (eleventyConfig) {
     collection.getFilteredByGlob("src/blog/posts/**/*.md").reverse().slice(0, 5)
   );
 
-  eleventyConfig.addCollection('albums', function (collectionApi) {
-    const albumsDir = './src/albums/';
-
-    // Read the subdirectories within the albums directory
-    const albumDirs = fs.readdirSync(albumsDir, { withFileTypes: true })
-      .filter((dirent) => dirent.isDirectory())
-      .map((dirent) => dirent.name);
-
-    // Generate album data for each subdirectory
-    return albumDirs.map((albumDir) => {
-      const albumPath = path.join(albumsDir, albumDir);
-      const photos = fs.readdirSync(albumPath)
-        .filter((file) => file.endsWith('.jpg'))
-        .map((file) => {
-          const photoPath = path.join(albumPath, file);
-          return {
-            url: `/${path.relative('./src', photoPath)}`,
-            alt: file.replace('.jpg', ''),
-          };
-        });
-
-      return {
-        url: `/${path.relative('./src', albumPath)}/index.html`,
-        data: {
-          title: albumDir,
-          photos,
-        },
-      };
-    });
-  });
-
   return {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "docs",
+      output: "_site",
     },
     templateFormats: ["md", "njk", "html"],
     markdownTemplateEngine: "njk",
